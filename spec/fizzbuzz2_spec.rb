f65440b9c520e5627729faf4e09e74bc23f12265
camilla <cgreene553@gmail.com>
2022-01-19 15:55:46 +0000

    Added argument

--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -1,6 +1,6 @@
 require 'fizzbuzz2'
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed multiples of 3' do
-    expect (fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
